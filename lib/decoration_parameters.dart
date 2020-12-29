44af13e5a663e7efe74ab8f9e50c9389bef4a5de
Vaibhav Kushwaha <vaibhav20092000@gmail.com>
2020-12-30 00:40:14 +0530

    added parameters for text style in tile displaying the cases of states

--- lib/decoration_parameters.dart
+++ lib/decoration_parameters.dart
@@ -4,3 +4,24 @@ const decoration_for_cases = BoxDecoration(
   borderRadius: BorderRadius.all(Radius.circular(10)),
   // color: Colors.orangeAccent,
 );
+
+const stateCaseTileParameter = BoxDecoration(
+  color: Colors.white,
+  borderRadius: BorderRadius.all(Radius.circular(6)),
+  boxShadow: [
+    BoxShadow(
+      color: Colors.black26,
+      spreadRadius: 3,
+      blurRadius: 5,
+    ),
+  ]
+);
+
+const stateTileTitleTextStyle = TextStyle(
+  fontWeight: FontWeight.bold,
+  fontSize: 20,
+);
+
+const stateTileSubtitleTextStyle = TextStyle(
+  fontSize: 15,
+);
